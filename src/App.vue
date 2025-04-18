9fd5536bfaa86cc03cbe62030b4c34fd1ecad335
lichang <dotb116393@163.com>
2025-04-18 11:00:12 +0800

    :new: add tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,7 @@
 </template>
 
 <script setup lang="ts"></script>
+
+<style>
+  @import 'tailwindcss';
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,25 @@ import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
+import tailwindcss from '@tailwindcss/vite'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    tailwindcss(),
     // 自动引入依赖
     AutoImport({
       imports: ['vue', 'vue-router'],
       dts: './src/auto-imports.d.ts'
     })
   ],
+  css: {
+    postcss: {
+      plugins: [autoprefixer]
+    }
+  },
   resolve: {
     // 配置别名
     alias: {
