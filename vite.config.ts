f87ff5dc93fb82769862dfb9ed6c1f011376b023
lichang <dotb116393@163.com>
2025-04-18 10:42:39 +0800

    :heavy_plus_sign: add basic modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // 自动引入依赖
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dts: './src/auto-imports.d.ts'
+    })
+  ],
+  resolve: {
+    // 配置别名
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
